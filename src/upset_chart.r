39c70f77c423041be0c463bdf6ce8d8109ed5d80
agonzalezbet <agonzalezbet@uoc.edu>
2025-10-28 13:26:11 +0100

    Add files via upload

--- src/upset_chart.r
+++ src/upset_chart.r
@@ -1,15 +1,24 @@
+# Cargamos las librerías necesarias para ejecutar todo el código
 library(UpSetR)
 library(grid)
 
+# Importamos los datos
+path <- "body_habits.csv"
+body_habits <- read_csv(path) 
 
+# Filtramos por individuos vegetarianos y separand también por géneros
 body_habits_vegetarian_female <- body_habits[body_habits$diet_type == "Vegetarian" & body_habits$Gender == "Female",]
 body_habits_vegetarian_male <- body_habits[body_habits$diet_type == "Vegetarian" & body_habits$Gender == "Male",]
+
+# Creamos una lista con 4 sublistas, una por cada tipo de entrenamiento, y dichas sublistas contienen las mujeres vegetarianas que practican ese tipo de entrenamiento
 x_female <- list(
     Yoga = sample(body_habits_vegetarian_female$ID,nrow(body_habits_vegetarian_female[body_habits_vegetarian_female$Workout_Type == "Yoga",])),
     HIIT = sample(body_habits_vegetarian_female$ID,nrow(body_habits_vegetarian_female[body_habits_vegetarian_female$Workout_Type == "HIIT",])),
     Strength = sample(body_habits_vegetarian_female$ID,nrow(body_habits_vegetarian_female[body_habits_vegetarian_female$Workout_Type == "Strength",])),
     Cardio = sample(body_habits_vegetarian_female$ID,nrow(body_habits_vegetarian_female[body_habits_vegetarian_female$Workout_Type == "Cardio",]))
 )
+
+# Creamos una lista con 4 sublistas, una por cada tipo de entrenamiento, y dichas sublistas contienen los hombres vegetarianos que practican ese tipo de entrenamiento
 x_male <- list(
     Yoga = sample(body_habits_vegetarian_male$ID,nrow(body_habits_vegetarian_male[body_habits_vegetarian_male$Workout_Type == "Yoga",])),
     HIIT = sample(body_habits_vegetarian_male$ID,nrow(body_habits_vegetarian_male[body_habits_vegetarian_male$Workout_Type == "HIIT",])),
@@ -17,6 +26,7 @@ x_male <- list(
     Cardio = sample(body_habits_vegetarian_male$ID,nrow(body_habits_vegetarian_male[body_habits_vegetarian_male$Workout_Type == "Cardio",]))
 )
 
+# Realizamos los gráficos UpSet
 upset(fromList(x_female), 
       order.by = "freq",
       main.bar.color = "orange",
